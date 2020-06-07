845c0843ea5264119308e89c821e67044e1e0b8b
Vinay M <Vinay.M@lntinfotech.com>
2020-06-07 15:54:09 +0530

    express-static-middleware

--- config/express.js
+++ config/express.js
@@ -11,11 +11,12 @@ methodOverride = require('method-override');
         } else if (process.env.NODE_ENV === 'production') { 
             app.use(compress()); 
         }
-        app.use(bodyParser.urlencoded({ extended: true  }));
+         app.use(bodyParser.urlencoded({ extended: true  }));
          app.use(bodyParser.json());
          app.use(methodOverride());
          app.set('views', './app/views');
          app.set('view engine', 'ejs');
      require('../app/routes/index.server.routes.js')(app);
+     app.use(express.static('./public'));
       return app; 
 }; 
